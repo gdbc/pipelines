7a8a288808bfc0ecaa58d43ebf9f1b0f59d2c8b7
graeme <graemedbc@gmail.com>
2018-11-08 09:45:36 -0500

    adding scp.py

---
+++ ol-slave/scp.py
@@ -0,0 +1,16 @@
+import sys
+import paramiko
+
+
+hostname = sys.argv['SSH_HOST']
+password = sys.argv['SSH_HOST_PASS'] 
+username = sys.argv['SSH_HOST_USER']
+port = 22
+
+localpath='/tmp/id_rsa'
+remotepath='/home/' + username + '/.ssh/id_rsa'
+
+t = paramiko.Transport((hostname, 22))
+t.connect(username=username, password=password)
+sftp = paramiko.SFTPClient.from_transport(t)
+sftp.get(remotepath,localpath)
