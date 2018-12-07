31ccc45d47e7481dcf608cbe2a797244dfb549b7
Cloud User <centos@dhost.example.local>
2018-12-07 16:50:00 -0500

    adding updated fabric file

--- flask-api/rq/my_fabric.py
+++ flask-api/rq/my_fabric.py
@@ -1,8 +1,9 @@
 from fabric2 import Connection, Config
 
-config = Config(overrides={'sudo': {'password': '<password>'}})
+config = Config(overrides={'sudo': {'password': 'test'}})
 
-c = Connection(host='server', user='test', port=22, config=config, connect_kwargs={"password": "<password>"})
-c.sudo('whoami', pty=True, hide='stderr')
-c.result
-help(c)
+c = Connection(host='server', user='test', port=22, config=config, connect_kwargs={"password": "test"})
+result = c.sudo('whoami', pty=True, hide='stdout')
+print(result.exited)
+print(result.ok)
+#print(result.stdout.strip())
