fd39a25b70d7b58545747b2864c00aa1e98d4482
Cloud User <centos@dhost.example.local>
2018-12-07 16:50:54 -0500

    updated fabric file

--- flask-api/rq/my_fabric.py
+++ flask-api/rq/my_fabric.py
@@ -1,8 +1,8 @@
 from fabric2 import Connection, Config
 
-config = Config(overrides={'sudo': {'password': 'test'}})
+config = Config(overrides={'sudo': {'password': '<password>'}})
 
-c = Connection(host='server', user='test', port=22, config=config, connect_kwargs={"password": "test"})
+c = Connection(host='server', user='test', port=22, config=config, connect_kwargs={"password": "<password>"})
 result = c.sudo('whoami', pty=True, hide='stdout')
 print(result.exited)
 print(result.ok)
