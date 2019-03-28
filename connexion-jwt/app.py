7f0b7424065d44cf97c09a202776e3ade9e844ce
Cloud User <centos@dhost.example.local>
2019-03-28 16:28:38 -0400

    added bu_rm

--- connexion-jwt/app.py
+++ connexion-jwt/app.py
@@ -111,7 +111,7 @@ def add_user_envs(user_id, env_id):
         env = Env.query.filter_by(env_name=env_id).first()
         user.envs.append(env)
         db.session.commit()
-        db.session.flush()
+        #db.session.flush()
         return "done"
     except Exception as e:
         return "Error: ",e
@@ -123,7 +123,7 @@ def del_user_envs(user_id, env_id):
         env = Env.query.filter_by(env_name=env_id).first()
         user.envs.remove(env)
         db.session.commit()
-        db.session.flush()
+        #db.session.flush()
         return "done"
     except Exception as e:
         return "Error: ",e
@@ -135,7 +135,7 @@ def add_user_bus(user_id, bu_id):
         bu = Bu.query.filter_by(bu_name=bu_id).first()
         user.bus.append(bu)
         db.session.commit()
-        db.session.flush() 
+        #db.session.flush() 
         return "done"
     except Exception as e:
         return "Error: ",e
@@ -146,7 +146,7 @@ def del_user_bus(user_id, bu_id):
         bu = Bu.query.filter_by(bu_name=bu_id).first()
         user.bus.remove(bu)
         db.session.commit()
-        db.session.flush()
+        #db.session.flush()
         return "done"
     except Exception as e:
         return "Error: ",e
@@ -157,7 +157,7 @@ def add_user_app(user_id, app_id):
         apps = App.query.filter_by(app_name=app_id).first()
         user.apptype.append(apps)
         db.session.commit()
-        db.session.flush()
+        #db.session.flush()
         return "done"
     except Exception as e:
         return "Error: ",e
@@ -169,7 +169,7 @@ def del_user_app(user_id, app_id):
         apps = App.query.filter_by(app_name=app_id).first()
         user.apptype.remove(apps)
         db.session.commit()
-        db.session.flush()
+        #db.session.flush()
         return "done"
     except Exception as e:
         return "Error: ",e
@@ -180,7 +180,7 @@ def add_user_host(user_id, host_id):
         host = Host.query.filter_by(host_name=host_id).first()
         user.host.append(host)
         db.session.commit()
-        db.session.flush()
+        #db.session.flush()
         return "done"
     except Exception as e:
         return "Error: ",e
@@ -192,7 +192,7 @@ def del_user_host(user_id, host_id):
         host = Host.query.filter_by(host_name=host_id).first()
         user.host.remove(host)
         db.session.commit()
-        db.session.flush()
+        #db.session.flush()
         return "done"
     except Exception as e:
         return "Error: ",e
@@ -267,7 +267,6 @@ def createdb():
     bu_it = Bu(bu_name='it')
     bu_dt = Bu(bu_name='dt')
     bu_rm = Bu(bu_name='rm')
-
     db.session.add(bu_it)
     db.session.add(bu_dt)
     db.session.add(bu_rm)
@@ -320,7 +319,7 @@ def createdb():
     user3.host.append(host11)
 
     db.session.commit()
-    db.session.flush()
+    #db.session.flush()
     
     return "done"
  
