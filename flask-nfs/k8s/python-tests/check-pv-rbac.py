616bafd88ffb99ddcf59333fcb1ec1eb3e5631a2
gdbc <gdbc@localhost.localdomain>
2020-03-11 13:55:22 +0000

    adding rbac check

--- flask-nfs/k8s/python-tests/check-pv-rbac.py
+++ flask-nfs/k8s/python-tests/check-pv-rbac.py
@@ -2,22 +2,27 @@ import yaml
 
 server="192.168.1.67"
 mount="/nfs/k8s/inttest1"
+#token="123asdffdaTGyI123zZ1"
+token="456asdffdaTGyI123zZ1"
 
-def checkrbac(svr, mnt, rbackfile="pv-rbac.yaml"):
+def checkrbac(token, svr, mnt, rbackfile="pv-rbac.yaml"):
    try: 
        rbacfile = yaml.load(open(rbackfile))
-       if svr in rbacfile['volumes']:
+       if token in rbacfile['tokens']:
            print("here")
-           print("paths: %s", rbacfile['volumes'][svr]['paths'])
-           if mnt in rbacfile['volumes'][svr]['paths']:
-               print("here1")
-               return True
+           if svr in rbacfile['tokens'][token]['volumes']:
+               print("hereW")
+               if mnt in rbacfile['tokens'][token]['volumes'][svr]['paths']:
+                   return True
+               else:
+                   return False
            else:
                return False
        else:
-           return True
+           return False
    except Exception as e:
        print("Error: Check authorizations: ", e)
        return False
 
-checkrbac(server, mount)
+res = checkrbac(token, server, mount)
+print(res)
