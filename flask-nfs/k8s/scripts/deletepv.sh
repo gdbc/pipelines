fa18d33dba3e36bc949dc95809c57aace49cf614
gdbc <gdbc@localhost.localdomain>
2020-03-17 22:27:55 +0000

    fixing type

--- flask-nfs/k8s/scripts/deletepv.sh
+++ flask-nfs/k8s/scripts/deletepv.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 cluster=$1
-pvanme=$2
+pvname=$2
 
 
 curl http://192.168.39.179:32000/deletepvs?token=123asdffdaTGyI123zZ1'&'pvname="$pvname"'&'cluster=$cluster
+
+echo "curl http://192.168.39.179:32000/deletepvs?token=123asdffdaTGyI123zZ1'&'pvname="$pvname"'&'cluster=$cluster"
 
 #curl http://localhost:5001/deletepvs?token=123asdffdaTGyI123zZ1'&'pvname="pv-inttest1"'&'cluster=minikube
 
--- flask-nfs/k8s/scripts/deletepvc.sh
+++ flask-nfs/k8s/scripts/deletepvc.sh
@@ -5,3 +5,5 @@ namespace=$2
 pvcname=$3
 
 curl http://192.168.39.179:32000/deletepvcs?token=123asdffdaTGyI123zZ1'&'pvcname="$pvcname"'&'namespace=$namespace'&'cluster=$cluster
+
+echo "curl http://192.168.39.179:32000/deletepvcs?token=123asdffdaTGyI123zZ1'&'pvcname="$pvcname"'&'namespace=$namespace'&'cluster=$cluster"
